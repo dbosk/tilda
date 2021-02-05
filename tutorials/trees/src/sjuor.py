de0d31a6689ede246f08b0bb31cd5b04ef248975
Daniel Bosk <dbosk@kth.se>
2021-02-05 12:08:56 +0100

    Fixes typos in sjuor.py

--- tutorials/trees/src/sjuor.py
+++ tutorials/trees/src/sjuor.py
@@ -1,20 +1,22 @@
 from queue import Queue
 
-def make_children(number, string):
+def make_children(queue):
+    number, string = queue.get()
+
     if number == 100:
        return True, string
 
-    q.put(tuple(tal+7, f"{string}+7"))
-    q.put(tuple(tal-7, f"{string}-7"))
-    q.put(tuple(tal*7, f"{string}*7"))
-    if tal%7 == 0:
-        q.put(tuple(tal/7, f"{string}/7"))
+    queue.put((number+7, f"({string})+7"))
+    queue.put((number-7, f"({string})-7"))
+    queue.put((number*7, f"({string})*7"))
+    if number%7 == 0:
+        queue.put((number/7, f"({string})/7"))
 
     return False, None
 
 def main():
     queue = Queue()
-    queue.put(0)
+    queue.put((0, "0"))
 
     found = False
     while queue and not found:
